15324c080f5ae4a9f2ca44350a12e4ff2aa3bb0c
lukaszkwasny <lukasz.kwasny94@gmail.com>
2020-06-25 00:30:25 +0200

    Add new function *reset *max/min *startNumber

--- 5isCircle/main.js
+++ 5isCircle/main.js
@@ -1,7 +1,20 @@
 const btn = document.querySelector("button");
+const btnReset = document.getElementById("reset");
 let number = 1;
+const input = document.getElementById("startNumber");
+var max = document.getElementById("startNumber").max;
 
-const addElement = function () {
+const starNumber = () => {
+
+    let inputValue = document.querySelector("input").value;
+    number = inputValue;
+    console.log(inputValue);
+
+};
+
+
+
+const addElement = () => {
 
     const div = document.createElement("div");
     div.textContent = number;
@@ -9,10 +22,33 @@ const addElement = function () {
         div.classList.add('circle');
     }
     document.body.appendChild(div);
+
     number++;
 
+    input.style.visibility = "hidden";
+
+    if (div.textContent == max) {
+        btn.style.visibility = "hidden";
+    };
 }
+
+const reset = () => {
+    const allDiv = document.querySelectorAll("div");
+
+
+    for (let i = 0; i < allDiv.length; i++) {
+        allDiv[i].remove();
+    };
+    number = 1;
+    input.style.visibility = "visible";
+    btn.style.visibility = "visible";
+    input.value = "1";
+};
+
+
 
 
 
 btn.addEventListener("click", addElement);
+btnReset.addEventListener("click", reset);
+input.addEventListener("change", starNumber);
