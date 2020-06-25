0da5639648c204f04cabda75ed7d7e07bed0fcd1
lukaszkwasny <lukasz.kwasny94@gmail.com>
2020-06-25 23:34:11 +0200

    change color h1

--- ChangeBackgroundcolor_posytionMouse/main.js
+++ ChangeBackgroundcolor_posytionMouse/main.js
@@ -3,4 +3,10 @@ document.body.addEventListener('mousemove', function (e) {
 
   h1.textContent = e.clientX + ", " + e.clientY;
   document.body.style.backgroundColor = `rgb(${e.clientX/3}, ${e.clientX/2}, ${e.clientX / e.clientY * 20 })`;
+
+  if (e.clientX < 500) {
+    h1.style.color = "white";
+  } else {
+    h1.style.color = "black";
+  };
 });
