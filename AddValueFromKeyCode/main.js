102c68e5189f210b8ef3d741879a67e76028b1fc
lukaszkwasny <lukasz.kwasny94@gmail.com>
2020-06-25 23:29:21 +0200

    add text in h1 and new div

--- AddValueFromKeyCode/main.js
+++ AddValueFromKeyCode/main.js
@@ -5,35 +5,47 @@ let blue = 100;
 document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
 
 const changeColor = (e) => {
-  console.log(e.keyCode, e.which);
   // 38 - ArrowUp
-  // 40 - ArrowDown
-
+  // 40 - ArrowDowd
 
   if (e.keyCode == 38 && red < 255) {
-    red = red + 10;
-    green = green + 10;
-    blue = blue + 10;
+    red = red + 5;
+    green = green + 5;
+    blue = blue + 5;
     document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+
   } else if (e.keyCode == 40 && red >= 0) {
-    red -= 10;
-    green -= 10;
-    blue -= 10;
-    document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+    if (red > 0) {
+      red -= 5;
+      green -= 5;
+      blue -= 5;
+      document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+    }
   };
 
+  const div = document.querySelector("div");
+  div.innerHTML = `rgb(${red}, ${green}, ${blue})`;
+  const h1 = document.querySelector("h1");
 
-
-  // switch (e.keyCode) {
-  //   case 38:
-  //     document.body.style.backgroundColor = `rgb(${red < 255 ? ++red : red }, ${green <= 255 ? ++green : green}, ${blue <= 255 ? ++blue : blue})`;
-  //     break;
-  //   case 40:
-  //     document.body.style.backgroundColor = `rgb(${red > 0 ? --red : red }, ${green > 0 ? --green : green}, ${blue > 0 ? --blue : blue})`;
-  //     break;
-
-  // }
-
+  if (red < 80 && green < 80 && blue < 80) {
+    div.style.color = "white";
+    h1.style.color = "white";
+  } else {
+    div.style.color = "black";
+    h1.style.color = "black";
+  };
 };
+
+// switch (e.keyCode) {
+//   case 38:
+//     document.body.style.backgroundColor = `rgb(${red < 255 ? ++red : red }, ${green <= 255 ? ++green : green}, ${blue <= 255 ? ++blue : blue})`;
+//     break;
+//   case 40:
+//     document.body.style.backgroundColor = `rgb(${red > 0 ? --red : red }, ${green > 0 ? --green : green}, ${blue > 0 ? --blue : blue})`;
+//     break;
+
+// }
+
+
 
 window.addEventListener('keydown', changeColor);
